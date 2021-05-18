cedfafe4ed013134f0dfed2c69a895477015e0e8
TELEMESSAGE\moti <moti@telemessage.com>
2021-05-18 11:43:23 +0300

    Fixed crash in Signal.

--- libsignal/service/src/main/java/org/whispersystems/signalservice/internal/contacts/crypto/Quote.java
+++ libsignal/service/src/main/java/org/whispersystems/signalservice/internal/contacts/crypto/Quote.java
@@ -108,7 +108,7 @@ public class Quote {
   }
 
   private void readFromBuffer(ByteBuffer quoteBuf, int pos, byte[] buf){
-    quoteBuf.position(pos);
+    (((Buffer)quoteBuf).position(pos);
     quoteBuf.get(buf);
   }
 
