76e381f7c5c62782ff179d3c76fca9b66c77409f
TELEMESSAGE\Shilo <shilo@telemessage.com>
2023-06-29 13:09:55 +0300

    Release_6.22.5.5

--- app/src/main/java/org/tm/archive/messages/WebsocketStrategy.java
+++ app/src/main/java/org/tm/archive/messages/WebsocketStrategy.java
@@ -21,81 +21,81 @@ import java.util.concurrent.TimeUnit;
  */
 public class WebSocketStrategy extends MessageRetrievalStrategy {
 
-  private static final String TAG = Log.tag(WebSocketStrategy.class);
+    private static final String TAG = Log.tag(WebSocketStrategy.class);
 
-  private static final String KEEP_ALIVE_TOKEN = "WebsocketStrategy";
-  private static final long   QUEUE_TIMEOUT = TimeUnit.SECONDS.toMillis(30);
+    private static final String KEEP_ALIVE_TOKEN = "WebsocketStrategy";
+    private static final long   QUEUE_TIMEOUT = TimeUnit.SECONDS.toMillis(30);
 
-  @WorkerThread
-  @Override
-  public boolean execute() {
-    Stopwatch               stopwatch = new Stopwatch("websocket-strategy");
-    IncomingMessageObserver observer  = ApplicationDependencies.getIncomingMessageObserver();
+    @WorkerThread
+    @Override
+    public boolean execute() {
+        Stopwatch               stopwatch = new Stopwatch("websocket-strategy");
+        IncomingMessageObserver observer  = ApplicationDependencies.getIncomingMessageObserver();
 
-    observer.registerKeepAliveToken(KEEP_ALIVE_TOKEN);
-    try {
-      JobManager              jobManager    = ApplicationDependencies.getJobManager();
-      QueueFindingJobListener queueListener = new QueueFindingJobListener();
+        observer.registerKeepAliveToken(KEEP_ALIVE_TOKEN);
+        try {
+            JobManager              jobManager    = ApplicationDependencies.getJobManager();
+            QueueFindingJobListener queueListener = new QueueFindingJobListener();
 
-      jobManager.addListener(job -> job.getParameters().getQueue() != null && job.getParameters().getQueue().startsWith(PushProcessMessageJob.QUEUE_PREFIX), queueListener);
+            jobManager.addListener(job -> job.getParameters().getQueue() != null && job.getParameters().getQueue().startsWith(PushProcessMessageJob.QUEUE_PREFIX), queueListener);
 
-      blockUntilWebsocketDrained(observer);
-      stopwatch.split("decryptions-drained");
+            blockUntilWebsocketDrained(observer);
+            stopwatch.split("decryptions-drained");
 
-      Set<String> processQueues = queueListener.getQueues();
-      Log.d(TAG, "Discovered " + processQueues.size() + " queue(s): " + processQueues);
+            Set<String> processQueues = queueListener.getQueues();
+            Log.d(TAG, "Discovered " + processQueues.size() + " queue(s): " + processQueues);
 
-      for (String queue : processQueues) {
-        blockUntilJobQueueDrained(queue, QUEUE_TIMEOUT);
-      }
+            for (String queue : processQueues) {
+                blockUntilJobQueueDrained(queue, QUEUE_TIMEOUT);
+            }
 
-      stopwatch.split("process-drained");
-      stopwatch.stop(TAG);
+            stopwatch.split("process-drained");
+            stopwatch.stop(TAG);
 
-      return true;
-    } finally {
-      ApplicationDependencies.getIncomingMessageObserver().removeKeepAliveToken(KEEP_ALIVE_TOKEN);
-    }
-  }
-
-  private static void blockUntilWebsocketDrained(IncomingMessageObserver observer) {
-    CountDownLatch latch = new CountDownLatch(1);
-
-    observer.addDecryptionDrainedListener(new Runnable() {
-      @Override public void run() {
-        latch.countDown();
-        observer.removeDecryptionDrainedListener(this);
-      }
-    });
-
-    try {
-      if (!latch.await(1, TimeUnit.MINUTES)) {
-        Log.w(TAG, "Hit timeout while waiting for decryptions to drain!");
-      }
-    } catch (InterruptedException e) {
-      Log.w(TAG, "Interrupted!", e);
-    }
-  }
-
-  private static void blockUntilJobQueueDrained(@NonNull String queue, long timeoutMs) {
-    long             startTime  = System.currentTimeMillis();
-    final JobManager jobManager = ApplicationDependencies.getJobManager();
-    final MarkerJob  markerJob  = new MarkerJob(queue);
-
-    Optional<JobTracker.JobState> jobState = jobManager.runSynchronously(markerJob, timeoutMs);
-
-    if (!jobState.isPresent()) {
-      Log.w(TAG, "Timed out waiting for " + queue + " job(s) to finish!");
+            return true;
+        } finally {
+            ApplicationDependencies.getIncomingMessageObserver().removeKeepAliveToken(KEEP_ALIVE_TOKEN);
+        }
     }
 
-    long endTime  = System.currentTimeMillis();
-    long duration = endTime - startTime;
+    private static void blockUntilWebsocketDrained(IncomingMessageObserver observer) {
+        CountDownLatch latch = new CountDownLatch(1);
 
-    Log.d(TAG, "Waited " + duration + " ms for the " + queue + " job(s) to finish.");
-  }
+        observer.addDecryptionDrainedListener(new Runnable() {
+            @Override public void run() {
+                latch.countDown();
+                observer.removeDecryptionDrainedListener(this);
+            }
+        });
 
-  @Override
-  public @NonNull String toString() {
-    return Log.tag(WebSocketStrategy.class);
-  }
+        try {
+            if (!latch.await(1, TimeUnit.MINUTES)) {
+                Log.w(TAG, "Hit timeout while waiting for decryptions to drain!");
+            }
+        } catch (InterruptedException e) {
+            Log.w(TAG, "Interrupted!", e);
+        }
+    }
+
+    private static void blockUntilJobQueueDrained(@NonNull String queue, long timeoutMs) {
+        long             startTime  = System.currentTimeMillis();
+        final JobManager jobManager = ApplicationDependencies.getJobManager();
+        final MarkerJob  markerJob  = new MarkerJob(queue);
+
+        Optional<JobTracker.JobState> jobState = jobManager.runSynchronously(markerJob, timeoutMs);
+
+        if (!jobState.isPresent()) {
+            Log.w(TAG, "Timed out waiting for " + queue + " job(s) to finish!");
+        }
+
+        long endTime  = System.currentTimeMillis();
+        long duration = endTime - startTime;
+
+        Log.d(TAG, "Waited " + duration + " ms for the " + queue + " job(s) to finish.");
+    }
+
+    @Override
+    public @NonNull String toString() {
+        return Log.tag(WebSocketStrategy.class);
+    }
 }
