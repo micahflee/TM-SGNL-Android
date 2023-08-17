f6e6c2891f0142e1d671ec2a482c989ee4b6ec54
TELEMESSAGE\Shilo <shilo@telemessage.com>
2023-08-17 14:18:27 +0300

    continue

--- app/src/main/java/org/tm/archive/registration/fragments/EnterPhoneNumberFragment.java
+++ app/src/main/java/org/tm/archive/registration/fragments/EnterPhoneNumberFragment.java
@@ -325,7 +325,7 @@ public final class EnterPhoneNumberFragment extends LoggingFragment
       IntuneAuthManager.INSTANCE.showDialog(requireActivity(), this::startMdm);
     } //update app that intune signed failed: two cases. 1. try intune auth again  2. move to self auth
     else if(reason.contains(server) || reason.contains("Authentication failed")
-            || reason.contains("managerID")) { //try intune auth again
+            /*|| reason.contains("managerID")*/) { //try intune auth again
       PrefManager.setIntPref(requireContext(), IntuneAuthManager.MDM_Auth_Status_String,IntuneAuthManager.MdmAuthStatus.START_INTUNE_AUTH.ordinal());
       com.tm.logger.Log.d(TAG, "status auth is " + IntuneAuthManager.MdmAuthStatus.START_INTUNE_AUTH.ordinal());
       requireActivity().runOnUiThread(new Runnable() {
@@ -695,7 +695,7 @@ public final class EnterPhoneNumberFragment extends LoggingFragment
   }
 
   public void updatedSelfAuthenticatorDonePreference() {
-
+    com.tm.logger.Log.d("SelfAuthenticator", "updatedSelfAuthenticatorDonePreference ");
     SharedPreferences        preferences = ApplicationContext.getInstance().getSharedPreferences(SelfAuthenticatorManager.SELF_AUTHENTICATION_PREFERENCE_NAME, Context.MODE_PRIVATE);
     SharedPreferences.Editor editor      = preferences.edit();
     editor.putBoolean("isAlreadyDoneSelfAuthentication", true);
