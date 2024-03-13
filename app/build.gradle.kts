c3e50bb4da3e0374b084d9c0edcfefb336812596
TELEMESSAGE\Shilo <shilo@telemessage.com>
2024-03-13 15:25:39 +0200

    increase version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,9 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 //**TM_SA**//Start - Change the version code and version name upon the current version
-val canonicalVersionCode = 1345
-val canonicalVersionName = "6.44.3.5"
-val signal_teleMessage_version = "6.44.3.5"//Change this param in Jenkins builder and delete it.
+val canonicalVersionCode = 1346
+val canonicalVersionName = "6.44.3.6"
+val signal_teleMessage_version = "6.44.3.6"//Change this param in Jenkins builder and delete it.
 //**TM_SA**//end
 
 /*val canonicalVersionCode = 1376
