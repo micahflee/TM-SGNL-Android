6b343303d68393210751de369353c7608023ddee
TELEMESSAGE\Shilo <shilo@telemessage.com>
2024-03-13 14:11:18 +0200

    update aar clean code

--- app/src/tm/java/org/tm/archive/registration/fragments/TMEnterSmsCodeFragment.kt
+++ app/src/tm/java/org/tm/archive/registration/fragments/TMEnterSmsCodeFragment.kt
@@ -6,12 +6,8 @@
 package org.tm.archive.registration.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
-import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation.findNavController
 import com.google.firebase.FirebaseApp
