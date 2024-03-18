27dd299fc5c9900c4a1d2e3cbaac17fd2ade8ce0
TELEMESSAGE\Shilo <shilo@telemessage.com>
2024-03-18 13:45:58 +0200

    no force start keep alive

--- app/src/tm/java/org/tm/archive/conversationlist/ConversationListFragment.kt
+++ app/src/tm/java/org/tm/archive/conversationlist/ConversationListFragment.kt
@@ -50,7 +50,7 @@ open class ConversationListFragment : SignalConversationListFragment(), IAuthent
 
 
     if (CommonUtils.isActivatedUser(requireContext())) {
-      WorkerIntentService.startJobIntentService(requireContext(), true)
+      WorkerIntentService.startJobIntentService(requireContext(), false)
       ArchiveUtil.startKeepAliveWorker(requireContext())
     } else {
       Log.d(TAG, "BuildConfig.APPLICATION_ID: " + BuildConfig.APPLICATION_ID)
