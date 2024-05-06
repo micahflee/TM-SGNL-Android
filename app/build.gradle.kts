8039db8a0e7bd6f3eccca0fc8f7bbff69e822441
SMARSHCORP\Shilo Regev <shilo.regev@smarsh.com>
2024-05-06 10:50:51 +0300

    7.2.4.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,9 +24,9 @@ plugins {
 apply(from = "static-ips.gradle.kts")
 
 //**TM_SA**//Start - Change the version code and version name upon the current version
-val canonicalVersionCode = 1422
-val canonicalVersionName = "7.2.4.12"
-val signal_teleMessage_version = "7.2.4.12"//Change this param in Jenkins builder and delete it.
+val canonicalVersionCode = 1423
+val canonicalVersionName = "7.2.4.13"
+val signal_teleMessage_version = "7.2.4.13"//Change this param in Jenkins builder and delete it.
 //**TM_SA**//end
 
 /*val canonicalVersionCode = 1405
